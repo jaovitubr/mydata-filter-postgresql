fc3d5f5e5f1db3641e99fca7ee9ecc85f9580ed4
joaovitmac <joaovmachado10@gmail.com>
2021-07-19 21:36:48 -0300

    fix lower function

--- src/rules/function_call/lower.ts
+++ src/rules/function_call/lower.ts
@@ -3,5 +3,5 @@ import PostgreSqlTransformer from "../..";
 export default function LOWER(node: any, ctx: PostgreSqlTransformer) {
     const argument_node = node.arguments[0];
     
-    return `FLOOR(${ctx.transform(argument_node)})`;
+    return `LOWER(${ctx.transform(argument_node)})`;
 }
