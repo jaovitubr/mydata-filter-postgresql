4236bdf2c996c112e219786d9388df422cd972d3
joaovitmac <joaovmachado10@gmail.com>
2021-07-27 22:25:32 -0300

    fix equal operator

--- src/rules/operators/eq.ts
+++ src/rules/operators/eq.ts
@@ -1,5 +1,5 @@
 import { PostgreSqlTransformer } from "../..";
 
 export default function EQ(node: any, ctx: PostgreSqlTransformer) {
-    return `${ctx.transform(node.left)} == ${ctx.transform(node.right)}`
+    return `${ctx.transform(node.left)} = ${ctx.transform(node.right)}`
 }
